17d45a7433d4860a2522e499956650d4a5dbd896
oozdal <ozerozdal@gmail.com>
2023-05-21 14:04:15 -0400

    updating views.py fixes #1

--- src/django_twitter_api/post/views.py
+++ src/django_twitter_api/post/views.py
@@ -10,11 +10,21 @@ def index(request):
         if content:
             print('Content:', content)
 
-            auth = tweepy.OAuthHandler(settings.API_KEY, settings.API_KEY_SECRET)
-            auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
+            ############################################################################
+            # As far as I understand, the following approach does not work anymore
+            #auth = tweepy.OAuthHandler(settings.API_KEY, settings.API_KEY_SECRET)
+            #auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
 
-            api =  tweepy.API(auth)
-            api.update_status(status=content)
+            #api =  tweepy.API(auth)
+            #api.update_status(status=content)
+            #############################################################################      
+
+            client = tweepy.Client(consumer_key=settings.API_KEY,
+                                   consumer_secret=settings.API_KEY_SECRET,
+                                   access_token=settings.ACCESS_TOKEN,
+                                   access_token_secret=settings.ACCESS_TOKEN_SECRET)
+
+            response = client.create_tweet(text=content)
 
             return redirect('index')
 
